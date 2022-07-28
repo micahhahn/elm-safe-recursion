f10984e54470009694790722d42241c6c53f82b4
Micah Hahn <micah@noredink.com>
2022-07-28 18:08:36 -0400

    Add Fold docs

--- src/Recursion/Fold.elm
+++ src/Recursion/Fold.elm
@@ -4,7 +4,7 @@ module Recursion.Fold exposing
     , foldlArray, foldrArray
     )
 
-{-|
+{-| This module contains functions for folding common collections types that can contain recursive data structures.
 
 
 ## List
@@ -28,7 +28,25 @@ import Dict exposing (Dict)
 import Recursion exposing (..)
 
 
-{-| -}
+{-| Reduces a list that contains recursive types from left to right.
+
+    type RoseTree a
+        = Leaf a
+        | Node (List (RoseTree a))
+
+    countRoseTreeLeaves : RoseTree a -> Int
+    countRoseTreeLeaves =
+        runRecursion
+            (\tree ->
+                case tree of
+                    Leaf _ ->
+                        base 1
+
+                    Node trees ->
+                        foldlList recurse (+) 0 trees
+            )
+
+-}
 foldlList : (x -> Rec a b c) -> (c -> d -> d) -> d -> List x -> Rec a b d
 foldlList project fold init list =
     let
@@ -43,35 +61,60 @@ foldlList project fold init list =
     go list init
 
 
-{-| -}
+{-| Fold a list that contains recursive types from right to left.
+
+    type RoseTree a
+        = Leaf a
+        | Node (List (RoseTree a))
+
+    countRoseTreeLeaves : RoseTree a -> Int
+    countRoseTreeLeaves =
+        runRecursion
+            (\tree ->
+                case tree of
+                    Leaf _ ->
+                        base 1
+
+                    Node trees ->
+                        foldrList recurse (+) 0 trees
+            )
+
+Favor `foldlList` if possible as it will be more efficient.
+
+-}
 foldrList : (x -> Rec a b c) -> (c -> d -> d) -> d -> List x -> Rec a b d
 foldrList project fold init list =
     foldlList project fold init (List.reverse list)
 
 
+uncurry : (a -> b -> c) -> ( a, b ) -> c
 uncurry f ( a, b ) =
     f a b
 
 
-{-| -}
+{-| Fold a dictionary that contains recursive types in its value type in ascending order of keys.
+-}
 foldlDict : (comparable -> v -> Rec a b c) -> (c -> d -> d) -> d -> Dict comparable v -> Rec a b d
 foldlDict project fold init =
     Dict.toList >> foldlList (uncurry project) fold init
 
 
-{-| -}
+{-| Fold a dictionary that contains recursive types in its value type in descending order of keys.
+-}
 foldrDict : (comparable -> v -> Rec a b c) -> (c -> d -> d) -> d -> Dict comparable v -> Rec a b d
 foldrDict project fold init =
     Dict.toList >> foldrList (uncurry project) fold init
 
 
-{-| -}
+{-| Fold an array that contains recursive types from left to right.
+-}
 foldlArray : (x -> Rec a b c) -> (c -> d -> d) -> d -> Array x -> Rec a b d
 foldlArray project fold init =
     Array.toList >> foldlList project fold init
 
 
-{-| -}
+{-| Fold an array that contains recursive types from right to left.
+-}
 foldrArray : (x -> Rec a b c) -> (c -> d -> d) -> d -> Array x -> Rec a b d
 foldrArray project fold init =
     Array.toList >> foldrList project fold init
