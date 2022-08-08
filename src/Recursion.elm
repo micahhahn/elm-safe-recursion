dac5ff7a87992ea1e2ddd32039c4d998f9399059
Micah Hahn <micah.s.hahn@gmail.com>
2022-08-08 16:53:29 -0400

    Dead simple Rec structure

---
+++ src/Recursion.elm
@@ -0,0 +1,25 @@
+module Recursion exposing (Rec(..), runRecursion)
+
+
+type Rec a b
+    = Base b
+    | Recurse a (b -> Rec a b)
+
+
+runRecursion : (a -> Rec a b) -> a -> b
+runRecursion project init =
+    let
+        go step stack =
+            case step of
+                Base b ->
+                    case stack of
+                        [] ->
+                            b
+
+                        next :: rest ->
+                            go (next b) rest
+
+                Recurse a next ->
+                    go (project a) (next :: stack)
+    in
+    go (project init) []
