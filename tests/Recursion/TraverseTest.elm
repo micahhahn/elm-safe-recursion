dc1a177534336bbf1787d75bbafee3098f3ef68c
Micah Hahn <micah.s.hahn@gmail.com>
2022-09-01 00:37:41 -0400

    Add correctness tests for traversals

--- tests/Recursion/TraverseTest.elm
+++ tests/Recursion/TraverseTest.elm
@@ -1,191 +1,371 @@
 module Recursion.TraverseTest exposing (suite)
 
+import Array exposing (Array)
+import Dict exposing (Dict)
 import Expect
 import Recursion exposing (..)
 import Recursion.Traverse exposing (..)
 import Test exposing (..)
-import Array exposing (Array)
-import Dict exposing (Dict)
-
-type RoseTree a
-        = RTNode a (List (RoseTree a))
-
-bigRoseTree : RoseTree Int
-bigRoseTree = 
-    List.range 1 10000
-        |> List.map (\x -> RTNode x []) |> RTNode 0
-
-sequenceListMap : (a -> b) -> RoseTree a -> RoseTree b
-sequenceListMap f = 
-    runRecursion <| 
-        \(RTNode val list) -> 
-            sequenceList list |> map (RTNode (f val))
-
-sequenceListThenMap : (a -> b) -> RoseTree a -> RoseTree b
-sequenceListThenMap f = 
-    runRecursion <| 
-        \(RTNode val list) -> 
-            sequenceListThen list (RTNode (f val) >> base)
 
 
-traverseListMap : (a -> b) -> RoseTree a -> RoseTree b
-traverseListMap f = 
-    runRecursion <| 
-        \(RTNode val list) -> 
-            traverseList recurse list |> map (RTNode (f val))
+type ListTree a
+    = ListNode a (List (ListTree a))
 
-traverseListThenMap : (a -> b) -> RoseTree a -> RoseTree b
-traverseListThenMap f = 
-    runRecursion <| 
-        \(RTNode val list) -> 
-            traverseListThen recurse list (RTNode (f val) >> base)
 
-type HashTrie a
-    = HTLeaf a
-    | HTNode (Dict String (HashTrie a))
-
-bigHashTrie : HashTrie Int
-bigHashTrie = 
-    List.range 1 10000
-        |> List.map (\x -> (String.fromInt x, HTLeaf x))
-        |> Dict.fromList
-        |> HTNode
-
-sequenceDictMap : (a -> b) -> HashTrie a -> HashTrie b
-sequenceDictMap f = 
+sequenceListMap : (a -> b) -> ListTree a -> ListTree b
+sequenceListMap f =
     runRecursion <|
-        \hash -> 
-            case hash of
-                HTLeaf a -> base (HTLeaf (f a))
+        \(ListNode val list) ->
+            sequenceList list |> map (ListNode (f val))
 
-                HTNode dict -> 
-                    sequenceDict dict |> map HTNode
 
-sequenceDictThenMap : (a -> b) -> HashTrie a -> HashTrie b
-sequenceDictThenMap f = 
+sequenceListThenMap : (a -> b) -> ListTree a -> ListTree b
+sequenceListThenMap f =
     runRecursion <|
-        \hash -> 
-            case hash of
-                HTLeaf a -> base (HTLeaf (f a))
+        \(ListNode val list) ->
+            sequenceListThen list (ListNode (f val) >> base)
 
-                HTNode dict -> 
-                    sequenceDictThen dict (HTNode >> base)
 
-traverseDictMap : (a -> b) -> HashTrie a -> HashTrie b
-traverseDictMap f = 
+traverseListMap : (a -> b) -> ListTree a -> ListTree b
+traverseListMap f =
     runRecursion <|
-        \hash -> 
-            case hash of
-                HTLeaf a -> base (HTLeaf (f a))
+        \(ListNode val list) ->
+            traverseList recurse list |> map (ListNode (f val))
 
-                HTNode dict -> 
-                    traverseDict (\_ v -> recurse v) dict |> map HTNode
 
-traverseDictThenMap : (a -> b) -> HashTrie a -> HashTrie b
-traverseDictThenMap f = 
+traverseListThenMap : (a -> b) -> ListTree a -> ListTree b
+traverseListThenMap f =
     runRecursion <|
-        \hash -> 
-            case hash of
-                HTLeaf a -> base (HTLeaf (f a))
+        \(ListNode val list) ->
+            traverseListThen recurse list (ListNode (f val) >> base)
 
-                HTNode dict -> 
-                    traverseDictThen (\_ v -> recurse v) dict (HTNode >> base)
+
+type DictTree a
+    = DictNode a (Dict String (DictTree a))
+
+
+sequenceDictMap : (a -> b) -> DictTree a -> DictTree b
+sequenceDictMap f =
+    runRecursion <|
+        \(DictNode val dict) ->
+            sequenceDict dict |> map (DictNode (f val))
+
+
+sequenceDictThenMap : (a -> b) -> DictTree a -> DictTree b
+sequenceDictThenMap f =
+    runRecursion <|
+        \(DictNode val dict) ->
+            sequenceDictThen dict (DictNode (f val) >> base)
+
+
+traverseDictMap : (a -> b) -> DictTree a -> DictTree b
+traverseDictMap f =
+    runRecursion <|
+        \(DictNode val dict) ->
+            traverseDict (\_ v -> recurse v) dict |> map (DictNode (f val))
+
+
+traverseDictThenMap : (a -> b) -> DictTree a -> DictTree b
+traverseDictThenMap f =
+    runRecursion <|
+        \(DictNode val dict) ->
+            traverseDictThen (\_ v -> recurse v) dict (DictNode (f val) >> base)
+
 
 type ArrayTree a
-        = ATNode a (Array (ArrayTree a))
+    = ArrayNode a (Array (ArrayTree a))
 
-bigArrayTree : ArrayTree Int
-bigArrayTree = 
-    List.range 1 10000
-        |> List.map (\x -> ATNode x (Array.fromList [])) 
-        |> Array.fromList
-        |> ATNode 0
 
 sequenceArrayMap : (a -> b) -> ArrayTree a -> ArrayTree b
-sequenceArrayMap f = 
-    runRecursion <| 
-        \(ATNode val array) -> 
-            sequenceArray array |> map (ATNode (f val))
+sequenceArrayMap f =
+    runRecursion <|
+        \(ArrayNode val array) ->
+            sequenceArray array |> map (ArrayNode (f val))
+
 
 sequenceArrayThenMap : (a -> b) -> ArrayTree a -> ArrayTree b
-sequenceArrayThenMap f = 
-    runRecursion <| 
-        \(ATNode val array) -> 
-            sequenceArrayThen array (ATNode (f val) >> base)
+sequenceArrayThenMap f =
+    runRecursion <|
+        \(ArrayNode val array) ->
+            sequenceArrayThen array (ArrayNode (f val) >> base)
 
 
 traverseArrayMap : (a -> b) -> ArrayTree a -> ArrayTree b
-traverseArrayMap f = 
-    runRecursion <| 
-        \(ATNode val array) -> 
-            traverseArray recurse array |> map (ATNode (f val))
+traverseArrayMap f =
+    runRecursion <|
+        \(ArrayNode val array) ->
+            traverseArray recurse array |> map (ArrayNode (f val))
+
 
 traverseArrayThenMap : (a -> b) -> ArrayTree a -> ArrayTree b
-traverseArrayThenMap f = 
-    runRecursion <| 
-        \(ATNode val array) -> 
-            traverseArrayThen recurse array (ATNode (f val) >> base)
+traverseArrayThenMap f =
+    runRecursion <|
+        \(ArrayNode val array) ->
+            traverseArrayThen recurse array (ArrayNode (f val) >> base)
+
+
+type MaybeTree a
+    = MaybeNode a (Maybe (MaybeTree a))
+
+
+sequenceMaybeMap : (a -> b) -> MaybeTree a -> MaybeTree b
+sequenceMaybeMap f =
+    runRecursion <|
+        \(MaybeNode val array) ->
+            sequenceMaybe array |> map (MaybeNode (f val))
+
+
+sequenceMaybeThenMap : (a -> b) -> MaybeTree a -> MaybeTree b
+sequenceMaybeThenMap f =
+    runRecursion <|
+        \(MaybeNode val array) ->
+            sequenceMaybeThen array (MaybeNode (f val) >> base)
+
+
+traverseMaybeMap : (a -> b) -> MaybeTree a -> MaybeTree b
+traverseMaybeMap f =
+    runRecursion <|
+        \(MaybeNode val array) ->
+            traverseMaybe recurse array |> map (MaybeNode (f val))
+
+
+traverseMaybeThenMap : (a -> b) -> MaybeTree a -> MaybeTree b
+traverseMaybeThenMap f =
+    runRecursion <|
+        \(MaybeNode val array) ->
+            traverseMaybeThen recurse array (MaybeNode (f val) >> base)
+
+
+type ResultTree a
+    = ResultNode a (Result String (ResultTree a))
+
+
+sequenceResultMap : (a -> b) -> ResultTree a -> ResultTree b
+sequenceResultMap f =
+    runRecursion <|
+        \(ResultNode val array) ->
+            sequenceResult array |> map (ResultNode (f val))
+
+
+sequenceResultThenMap : (a -> b) -> ResultTree a -> ResultTree b
+sequenceResultThenMap f =
+    runRecursion <|
+        \(ResultNode val array) ->
+            sequenceResultThen array (ResultNode (f val) >> base)
+
+
+traverseResultMap : (a -> b) -> ResultTree a -> ResultTree b
+traverseResultMap f =
+    runRecursion <|
+        \(ResultNode val array) ->
+            traverseResult recurse array |> map (ResultNode (f val))
+
+
+traverseResultThenMap : (a -> b) -> ResultTree a -> ResultTree b
+traverseResultThenMap f =
+    runRecursion <|
+        \(ResultNode val array) ->
+            traverseResultThen recurse array (ResultNode (f val) >> base)
+
 
 stackSafetyTests : Test
-stackSafetyTests = 
+stackSafetyTests =
     describe "Stack Safety Tests"
-        [ describe "List" 
-            [ test "sequenceList doesn't overflow" <| 
-                \_ ->  
-                    let _ = sequenceListMap ((+) 1) bigRoseTree
-                    in Expect.pass
-            , test "sequenceListThen doesn't overflow" <| 
-                \_ ->  
-                    let _ = sequenceListThenMap ((+) 1) bigRoseTree
-                    in Expect.pass
-            , test "traverseList doesn't overflow" <| 
-                \_ ->  
-                    let _ = traverseListMap ((+) 1) bigRoseTree
-                    in Expect.pass
-            , test "traverseListThen doesn't overflow" <| 
-                \_ ->  
-                    let _ = traverseListThenMap ((+) 1) bigRoseTree
-                    in Expect.pass
-            ]
+        [ describe "List"
+            (let
+                bigListTree =
+                    List.range 1 10000
+                        |> List.map (\x -> ListNode x [])
+                        |> ListNode 0
+             in
+             [ test "sequenceList doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            sequenceListMap ((+) 1) bigListTree
+                    in
+                    Expect.pass
+             , test "sequenceListThen doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            sequenceListThenMap ((+) 1) bigListTree
+                    in
+                    Expect.pass
+             , test "traverseList doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            traverseListMap ((+) 1) bigListTree
+                    in
+                    Expect.pass
+             , test "traverseListThen doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            traverseListThenMap ((+) 1) bigListTree
+                    in
+                    Expect.pass
+             ]
+            )
         , describe "Dict"
-            [ test "sequenceDict doesn't overflow" <|
-                \_ ->  
-                    let _ = sequenceDictMap ((+) 1) bigHashTrie
-                    in Expect.pass
-            ,test "sequenceDictThen doesn't overflow" <|
-                \_ ->  
-                    let _ = sequenceDictThenMap ((+) 1) bigHashTrie
-                    in Expect.pass
-            , test "traverseDict doesn't overflow" <|
-                \_ ->  
-                    let _ = traverseDictMap ((+) 1) bigHashTrie
-                    in Expect.pass
-            , test "traverseDictThen doesn't overflow" <|
-                \_ ->  
-                    let _ = traverseDictThenMap ((+) 1) bigHashTrie
-                    in Expect.pass
-            ]
+            (let
+                bigDictTree =
+                    List.range 1 10000
+                        |> List.map (\x -> ( String.fromInt x, DictNode x Dict.empty ))
+                        |> Dict.fromList
+                        |> DictNode 0
+             in
+             [ test "sequenceDict doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            sequenceDictMap ((+) 1) bigDictTree
+                    in
+                    Expect.pass
+             , test "sequenceDictThen doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            sequenceDictThenMap ((+) 1) bigDictTree
+                    in
+                    Expect.pass
+             , test "traverseDict doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            traverseDictMap ((+) 1) bigDictTree
+                    in
+                    Expect.pass
+             , test "traverseDictThen doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            traverseDictThenMap ((+) 1) bigDictTree
+                    in
+                    Expect.pass
+             ]
+            )
         , describe "Array"
-            [ test "sequenceArray doesn't overflow" <| 
-                \_ ->  
-                    let _ = sequenceArrayMap ((+) 1) bigArrayTree
-                    in Expect.pass
-            , test "sequenceArrayThen doesn't overflow" <| 
-                \_ ->  
-                    let _ = sequenceArrayThenMap ((+) 1) bigArrayTree
-                    in Expect.pass
-            , test "traverseArray doesn't overflow" <| 
-                \_ ->  
-                    let _ = traverseArrayMap ((+) 1) bigArrayTree
-                    in Expect.pass
-            , test "traverseArrayThen doesn't overflow" <| 
-                \_ ->  
-                    let _ = traverseArrayThenMap ((+) 1) bigArrayTree
-                    in Expect.pass
-            ]
+            (let
+                bigArrayTree =
+                    List.range 1 10000
+                        |> List.map (\x -> ArrayNode x (Array.fromList []))
+                        |> Array.fromList
+                        |> ArrayNode 0
+             in
+             [ test "sequenceArray doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            sequenceArrayMap ((+) 1) bigArrayTree
+                    in
+                    Expect.pass
+             , test "sequenceArrayThen doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            sequenceArrayThenMap ((+) 1) bigArrayTree
+                    in
+                    Expect.pass
+             , test "traverseArray doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            traverseArrayMap ((+) 1) bigArrayTree
+                    in
+                    Expect.pass
+             , test "traverseArrayThen doesn't overflow" <|
+                \_ ->
+                    let
+                        _ =
+                            traverseArrayThenMap ((+) 1) bigArrayTree
+                    in
+                    Expect.pass
+             ]
+            )
         ]
 
+
+correctnessTests : Test
+correctnessTests =
+    describe "Correctness tests"
+        [ describe "List"
+            (let
+                initListTree =
+                    ListNode 2 [ ListNode 1 [], ListNode 3 [] ]
+
+                expectedListTree =
+                    ListNode 3 [ ListNode 2 [], ListNode 4 [] ]
+             in
+             [ test "sequenceList" <| \_ -> Expect.equal expectedListTree (sequenceListMap ((+) 1) initListTree)
+             , test "sequenceListThen" <| \_ -> Expect.equal expectedListTree (sequenceListThenMap ((+) 1) initListTree)
+             , test "traverseList" <| \_ -> Expect.equal expectedListTree (traverseListMap ((+) 1) initListTree)
+             , test "traverseListThen" <| \_ -> Expect.equal expectedListTree (traverseListThenMap ((+) 1) initListTree)
+             ]
+            )
+        , describe "Dict"
+            (let
+                initDictTree =
+                    DictNode 2 (Dict.fromList [ ( "1", DictNode 1 Dict.empty ), ( "3", DictNode 3 Dict.empty ) ])
+
+                expectedDictTree =
+                    DictNode 3 (Dict.fromList [ ( "1", DictNode 2 Dict.empty ), ( "3", DictNode 4 Dict.empty ) ])
+             in
+             [ test "sequenceDict" <| \_ -> Expect.equal expectedDictTree (sequenceDictMap ((+) 1) initDictTree)
+             , test "sequenceDictThen" <| \_ -> Expect.equal expectedDictTree (sequenceDictThenMap ((+) 1) initDictTree)
+             , test "traverseDict" <| \_ -> Expect.equal expectedDictTree (traverseDictMap ((+) 1) initDictTree)
+             , test "traverseDictThen" <| \_ -> Expect.equal expectedDictTree (traverseDictThenMap ((+) 1) initDictTree)
+             ]
+            )
+        , describe "Array"
+            (let
+                initArrayTree =
+                    ArrayNode 2 (Array.fromList [ ArrayNode 1 Array.empty, ArrayNode 3 Array.empty ])
+
+                expectedArrayTree =
+                    ArrayNode 3 (Array.fromList [ ArrayNode 2 Array.empty, ArrayNode 4 Array.empty ])
+             in
+             [ test "sequenceArray" <| \_ -> Expect.equal expectedArrayTree (sequenceArrayMap ((+) 1) initArrayTree)
+             , test "sequenceArrayThen" <| \_ -> Expect.equal expectedArrayTree (sequenceArrayThenMap ((+) 1) initArrayTree)
+             , test "traverseArray" <| \_ -> Expect.equal expectedArrayTree (traverseArrayMap ((+) 1) initArrayTree)
+             , test "traverseArrayThen" <| \_ -> Expect.equal expectedArrayTree (traverseArrayThenMap ((+) 1) initArrayTree)
+             ]
+            )
+        , describe "Maybe"
+            (let
+                initMaybeTree =
+                    MaybeNode 1 (Just <| MaybeNode 2 (Just <| MaybeNode 3 Nothing))
+
+                expectedMaybeTree =
+                    MaybeNode 2 (Just <| MaybeNode 3 (Just <| MaybeNode 4 Nothing))
+             in
+             [ test "sequenceMaybe" <| \_ -> Expect.equal expectedMaybeTree (sequenceMaybeMap ((+) 1) initMaybeTree)
+             , test "sequenceMaybeThen" <| \_ -> Expect.equal expectedMaybeTree (sequenceMaybeThenMap ((+) 1) initMaybeTree)
+             , test "traverseMaybe" <| \_ -> Expect.equal expectedMaybeTree (traverseMaybeMap ((+) 1) initMaybeTree)
+             , test "traverseMaybeThen" <| \_ -> Expect.equal expectedMaybeTree (traverseMaybeThenMap ((+) 1) initMaybeTree)
+             ]
+            )
+        , describe "Result"
+            (let
+                initResultTree =
+                    ResultNode 1 (Ok <| ResultNode 2 (Ok <| ResultNode 3 (Err "a")))
+
+                expectedResultTree =
+                    ResultNode 2 (Ok <| ResultNode 3 (Ok <| ResultNode 4 (Err "a")))
+             in
+             [ test "sequenceResult" <| \_ -> Expect.equal expectedResultTree (sequenceResultMap ((+) 1) initResultTree)
+             , test "sequenceResultThen" <| \_ -> Expect.equal expectedResultTree (sequenceResultThenMap ((+) 1) initResultTree)
+             , test "traverseResult" <| \_ -> Expect.equal expectedResultTree (traverseResultMap ((+) 1) initResultTree)
+             , test "traverseResultThen" <| \_ -> Expect.equal expectedResultTree (traverseResultThenMap ((+) 1) initResultTree)
+             ]
+            )
+        ]
+
+
 suite : Test
-suite = 
+suite =
     describe "Recursion.Traverse"
-        [ stackSafetyTests ]
+        [ stackSafetyTests
+        , correctnessTests
+        ]
